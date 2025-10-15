dce82dbf4ed96093b8430f8e10e7a5ce1a60a9da
amarghioali <88341119+amarghioali@users.noreply.github.com>
2025-10-16 00:36:34 +0300

    draft - home page carousel

--- ext/blocks/carousel/carousel.js
+++ ext/blocks/carousel/carousel.js
@@ -1,11 +1,9 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 import createSlider from '../../scripts/slider.js';
-import { isAuthoring} from '../../scripts/reference-limiter.js';
-
+import { isAuthoring } from '../../scripts/reference-limiter.js';
 
 export default function decorate(block) {
-
   const isBottomCarousel = block.classList.contains('carousel-with-button');
 
   let i = 0;
@@ -28,45 +26,43 @@ export default function decorate(block) {
       });
       slider.append(li);
     } else {
-        const contentEl = row;
-        if (contentEl) {
-          if(contentEl.id)
-          {
-             h2Element = contentEl.id;
-          }
-          const button = contentEl.querySelector('a.button, button');
-          if (button) {
-               button.setAttribute('aria-describedby', h2Element);
-               buttonMobileElement.appendChild(button.cloneNode(true));
-          }
-
-          // Append the rest of the content to leftContent
-          leftContent.append(contentEl);
+      const contentEl = row;
+      if (contentEl) {
+        if (contentEl.id) {
+          h2Element = contentEl.id;
+        }
+        const button = contentEl.querySelector('a.button, button');
+        if (button) {
+          button.setAttribute('aria-describedby', h2Element);
+          buttonMobileElement.appendChild(button.cloneNode(true));
         }
 
-        leftContent.className = isBottomCarousel ? 'main-heading' : 'default-content-wrapper';
+        // Append the rest of the content to leftContent
+        leftContent.append(contentEl);
       }
+
+      leftContent.className = isBottomCarousel ? 'main-heading' : 'default-content-wrapper';
+    }
     i += 1;
   });
 
   // Optimise pictures
   slider.querySelectorAll('picture > img').forEach((img) => {
-    let alt = img.alt;
-    if(!alt)
-    {
-        alt = 'Default Alt';
+    let { alt } = img;
+    if (!alt) {
+      alt = 'Default Alt';
     }
-    if (!img.src.includes("delivery-")) {
-    const optimizedPic = createOptimizedPicture(img.src, alt, false, [{ width: '750' }]);
-    moveInstrumentation(img, optimizedPic.querySelector('img'));
-    img.closest('picture').replaceWith(optimizedPic);
+    if (!img.src.includes('delivery-')) {
+      const optimizedPic = createOptimizedPicture(img.src, alt, false, [{ width: '750' }]);
+      moveInstrumentation(img, optimizedPic.querySelector('img'));
+      img.closest('picture').replaceWith(optimizedPic);
     }
   });
 
   /* ---------- extra wrapping only for bottom carousel ---------- */
   if (isBottomCarousel) {
     slider.querySelectorAll('li').forEach((li) => {
-      const imgDiv  = li.querySelector('.cards-card-image');
+      const imgDiv = li.querySelector('.cards-card-image');
       const bodyDiv = li.querySelector('.cards-card-body');
       const oldLink = bodyDiv?.querySelector('a');
       if (!imgDiv || !bodyDiv || !oldLink) return;
@@ -92,36 +88,31 @@ export default function decorate(block) {
 
   // Replace original block content
   block.textContent = '';
-  if(!isBottomCarousel)
-  {
-      if (leftContent) {
-        block.parentNode.parentNode.prepend(leftContent);
-      }
-  }
-  else
-  {
+  if (!isBottomCarousel) {
+    if (leftContent) {
+      block.parentNode.parentNode.prepend(leftContent);
+    }
+  } else {
     block.appendChild(leftContent);
   }
   block.append(slider);
-  if(isBottomCarousel)
-  {
-   block.appendChild(buttonMobileElement);
+  if (isBottomCarousel) {
+    block.appendChild(buttonMobileElement);
   }
   createSlider(block);
-    const cardList = block.querySelector('ul');
-    cardList.setAttribute('tabindex', '-1');
-    const cards = cardList.querySelectorAll('li');
-    const MAX_CARDS = 7;
-    if (cards.length > MAX_CARDS) {
-
-           if (isAuthoring()) {
-              const warning = document.createElement('div');
-              warning.textContent = `⚠️ Only 7 cards are allowed. Extra cards have been ignored.`;
-              warning.style.color = 'red';
-              block.prepend(warning);
-           }
-
-          const extraCards = Array.from(cards).slice(MAX_CARDS);
-          extraCards.forEach(card => card.remove());
+  const cardList = block.querySelector('ul');
+  cardList.setAttribute('tabindex', '-1');
+  const cards = cardList.querySelectorAll('li');
+  const MAX_CARDS = 7;
+  if (cards.length > MAX_CARDS) {
+    if (isAuthoring()) {
+      const warning = document.createElement('div');
+      warning.textContent = '⚠️ Only 7 cards are allowed. Extra cards have been ignored.';
+      warning.style.color = 'red';
+      block.prepend(warning);
     }
+
+    const extraCards = Array.from(cards).slice(MAX_CARDS);
+    extraCards.forEach((card) => card.remove());
+  }
 }
