ec0c467e3ac0a2e1f0c80e5a0f2ead41fea7f8cc
iustinp <iustinp@adobe.com>
2025-10-30 17:37:03 +0200

    removed root editor-support.js, fixed iframe height, fixed gray background applying everywhere.

--- scripts/editor-support.js
+++
@@ -1,7 +0,0 @@
-/**
- * This file serves as a redirect/wrapper for the Universal Editor.
- * The actual editor-support logic is now located at /ext/scripts/editor-support.js
- * This file exists here because the Universal Editor loads editor-support.js
- * from /scripts/ by convention.
- */
-import '/content/wbcopy.resource/ext/scripts/editor-support.js';
