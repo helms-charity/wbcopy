4c68e78904119a01e1e9cd7b53659b4ab75b39e1
iustinp <iustinp@adobe.com>
2025-10-30 13:05:35 +0200

    moved nav and footer and pointing to them now. Also now pointing /scripts/editor-support.js to /ext/scripts/editor-support.js

--- ext/scripts/editor-support.js
+++ ext/scripts/editor-support.js
@@ -6,9 +6,9 @@ import {
   decorateSections,
   loadBlock,
   loadBlocks,
-} from './aem.js';
-import { decorateRichtext } from './editor-support-rte.js';
-import { decorateMain } from './scripts.js';
+} from '/content/wbcopy.resource/ext/scripts/aem.js';
+import { decorateRichtext } from '/content/wbcopy.resource/ext/scripts/editor-support-rte.js';
+import { decorateMain } from '/content/wbcopy.resource/ext/scripts/scripts.js';
 
 async function applyChanges(event) {
   // redecorate default content and blocks on patches (in the properties rail)
